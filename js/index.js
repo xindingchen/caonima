e0ec0b7fe6f2bdacb7da7c803e54473878a21d7e
xindingchen <xind1_15@uni.worc.ac.uk>
2016-10-20 14:55:35 +0100

    2

--- js/index.js
+++ js/index.js
@@ -8,38 +8,14 @@ var value="blue";
 		console.log("device ready");
     }
 	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
-
-
-    // device APIs are available
-    //
+	
     function onDeviceReady() {
-		alert("device ready");
-        window.localStorage.setItem( key,value);
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
-		
-		launched_count++;
-		updateDisplay();
+		window.localStorage.setItem( key, value );
         var key1="pen";
-        var value =window.localStorage.getItem(key1);
+        var value =window.localStorage.getItem(key);
         alert(value);
     }
 
     // Handle the pause event
     //
-    function onPause() {
-		alert("pause");
-		paused_count++;
-		updateDisplay();
-    }
-	
-	function onResume() {
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
+    
